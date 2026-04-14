905c81173445118bd7db30cfa13965d3e406361b
David <ds0196@uah.edu>
2026-04-14 16:01:58 -0500

    feat: add is_main, cleanup debug messages

--- extra_script.py
+++ extra_script.py
@@ -10,9 +10,12 @@ import os
 import sys
 from warnings import warn
 
+DEBUG = False
+
 
 def get_repo_name():
-    warn("PLEASE IGNORE THESE WARNINGS! They are not actually warnings. This text just won't show up with print(). Ask David ._.")
+    if DEBUG:
+        warn("PLEASE IGNORE THESE WARNINGS! They are not actually warnings. This text just won't show up with print(). Ask David ._.")
     repo_name = subprocess.run(["git", "rev-parse", "--show-toplevel"], capture_output=True, text=True)
     if repo_name.returncode != 0:
         warn("Warning: Could not determine git repository path.")
@@ -20,7 +23,8 @@ def get_repo_name():
     else:
         repo_path = repo_name.stdout.strip()
         repo_name_real = repo_path.split("/")[-1].split("\\")[-1]
-        warn(f"Current Git repository: {repo_name_real}")
+        if DEBUG:
+            warn(f"Current Git repository: {repo_name_real}")
         return repo_name_real
 
 def detect_versioning():
@@ -38,6 +42,13 @@ def detect_versioning():
         version_major = int(version_parts[0]) if len(version_parts) > 0 else 0
         version_minor = int(version_parts[1]) if len(version_parts) > 1 else 0
         version_patch = int(version_parts[2]) if len(version_parts) > 2 else 0
+    
+    # Set ismain to true if on main and not dirty
+    git_branch = subprocess.run(["git", "rev-parse", "--abbrev-ref", "HEAD"], capture_output=True, text=True)
+    if git_branch.returncode == 0 and git_branch.stdout.strip() == "main":
+        is_main = 1
+    else:
+        is_main = 0
 
     # Set isdirty to 1 if there are uncommitted changes
     git_status = subprocess.run(["git", "status", "--porcelain"], capture_output=True, text=True)
@@ -53,10 +64,11 @@ def detect_versioning():
         commit_hash = 0
     else:
         commit_hash = git_hash.stdout.strip()
-        warn(f"Current Git commit hash: {commit_hash}")
+        if DEBUG:
+            warn(f"Current Git commit hash: {commit_hash}")
         commit_hash = int(commit_hash, 16)  # convert hex string to integer
 
-    return (version_major, version_minor, version_patch, is_dirty, commit_hash)
+    return (version_major, version_minor, version_patch, is_main, is_dirty, commit_hash)
 
 
 def append_version_defines(defines):
@@ -68,21 +80,25 @@ def setup_lib_versioning(target=None, source=None, env=None):
     # First, check the repository name
     repo_name = get_repo_name()
 
-    if repo_name and repo_name.endswith("-Embedded-Lib"):
-        version_major, version_minor, version_patch, is_dirty, commit_hash = detect_versioning()
+    if repo_name and repo_name.lower().endswith("-embedded-lib"):
+        version_major, version_minor, version_patch, is_main, is_dirty, commit_hash = detect_versioning()
     else:
         warn("Repository name mismatch. crying.")
         version_major = 0
         version_minor = 0
         version_patch = 0
+        is_main = 0
         is_dirty = 0
         commit_hash = 0
 
-    warn(f"Got version for astra-embedded-lib: {version_major}.{version_minor}.{version_patch}, is_dirty={is_dirty}, hash={commit_hash}")
+    if DEBUG:
+        warn(f"Got version for astra-embedded-lib: {version_major}.{version_minor}.{version_patch}, is_main={is_main}, is_dirty={is_dirty}, hash={commit_hash}")
+
     append_version_defines([
         ("ASTRA_LIB_VERSION_MAJOR", version_major),
         ("ASTRA_LIB_VERSION_MINOR", version_minor),
         ("ASTRA_LIB_VERSION_PATCH", version_patch),
+        ("ASTRA_LIB_VERSION_ISMAIN", is_main),
         ("ASTRA_LIB_VERSION_ISDIRTY", is_dirty),
         ("ASTRA_LIB_VERSION_COMMIT_HASH", commit_hash)
     ])
@@ -92,20 +108,24 @@ def setup_project_versioning(target=None, source=None, env=None):
     repo_name = get_repo_name()
 
     if repo_name and "-embedded" in repo_name:
-        version_major, version_minor, version_patch, is_dirty, commit_hash = detect_versioning()
+        version_major, version_minor, version_patch, is_main, is_dirty, commit_hash = detect_versioning()
     else:
         warn("Repository name mismatch. crying.")
         version_major = 0
         version_minor = 0
         version_patch = 0
+        is_main = 0
         is_dirty = 0
         commit_hash = 0
 
-    warn(f"Got version for project: {version_major}.{version_minor}.{version_patch}, is_dirty={is_dirty}, hash={commit_hash}")
+    if DEBUG:
+        warn(f"Got version for project: {version_major}.{version_minor}.{version_patch}, is_main={is_main}, is_dirty={is_dirty}, hash={commit_hash}")
+
     append_version_defines([
         ("PROJECT_VERSION_MAJOR", version_major),
         ("PROJECT_VERSION_MINOR", version_minor),
         ("PROJECT_VERSION_PATCH", version_patch),
+        ("PROJECT_VERSION_ISMAIN", is_main),
         ("PROJECT_VERSION_ISDIRTY", is_dirty),
         ("PROJECT_VERSION_COMMIT_HASH", commit_hash)
     ])
@@ -116,7 +136,6 @@ def setup_project_versioning(target=None, source=None, env=None):
 def main():
     setup_lib_versioning()  # Called as a part of *-embedded-Lib build process
     old_wd = os.getcwd()
-    warn(f"Starting working directory: {old_wd}")
     os.chdir(global_env.subst("$BUILD_DIR"))
     setup_project_versioning()
     os.chdir(old_wd)
